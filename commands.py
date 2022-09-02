b9b199adf0f301ebb9925d9180be29cb2c260fdf
theoopsguy <theoopsguy01@gmail.com>
2022-09-02 13:49:30 +0530

    Read chat commands from element located by classname.

--- commands.py
+++ commands.py
@@ -13,52 +13,48 @@ def commands(driver):
     driver=driver
     # To wait until bot joins the meet.
     WebDriverWait(driver, 10000000).until(EC.visibility_of_element_located((By.ID,'new-toolbox')))
+
     # Open chat box
     chatIcon=driver.find_element(By.CLASS_NAME, 'toolbar-button-with-badge')
     ActionChains(driver).move_to_element(chatIcon).click().perform()
     driver.implicitly_wait(3)
+
     # To wait until text message box is visible
     WebDriverWait(driver, 10000000).until(EC.visibility_of_element_located((By.CLASS_NAME, 'icon-input')))
+
+    # Introduce bot in chat
     driver.find_element(By.CLASS_NAME, 'icon-input').send_keys("Hi! I'm Music bot." + Keys.ENTER)
     driver.implicitly_wait(3)
     print("Introduced in chat")
 
     #Read chat commands
-    i=1
+    msgIndex=1
     run=1
     while(run==1):
-        WebDriverWait(driver, 2000000000).until(EC.visibility_of_element_located((By.XPATH,'//*[@id="chatconversation"]/div['+str(i)+']/div/div[1]/div/div/div[2]')))
-        # WebDriverWait(driver, 2000000000).until(EC.visibility_of_element_located((By.CLASS_NAME, 'usermessage')))
-        chatText=driver.find_element(By.CLASS_NAME, 'usermessage').text
-        print("Here's the chat msg: "+chatText)
-        # For now not reading messages in group by the same person. Can implement later.
+        chatList=driver.find_elements(By.CLASS_NAME, 'usermessage')
+        if len(chatList)>msgIndex:
+            chatText=chatList[msgIndex].text
+            msgIndex=msgIndex+1
 
-        #Removing author details
-        actualChatText=str(chatText).split("\n",1)[1]
-        actualChatText=str(actualChatText).lower()
+            #Removing author details
+            actualChatText=str(chatText).split("\n",1)[1]
+            actualChatText=str(actualChatText).lower()
 
-        if "/play" in actualChatText:
-            print("Play Song")
-            songNameStartIdx=actualChatText.find("/play")+6
-            songName=actualChatText[songNameStartIdx:]
-            musicWindow=playSong(driver, songName)
-            i=i+1
-        elif "/pause" in actualChatText:
-            print("Pause Song")
-            pauseSong(driver, musicWindow)
-            i=i+1
-        elif "/resume" in actualChatText:
-            print("Resume song")
-            resumeSong(driver, musicWindow)
-            i=i+1
-        elif "/help" in actualChatText:
-            print("Bot help")
-            helpCommand(driver)
-            i=i+1
-        elif "/exit" in actualChatText:
-            run=0
-            print("Exit bot")
-            driver.quit()
-        else:
-            print(actualChatText)
-            i=i+1
+            if "/play" in actualChatText:
+                print("Play Song")
+                songNameStartIdx=actualChatText.find("/play")+6
+                songName=actualChatText[songNameStartIdx:]
+                musicWindow=playSong(driver, songName)
+            elif "/pause" in actualChatText:
+                print("Pause Song")
+                pauseSong(driver, musicWindow)
+            elif "/resume" in actualChatText:
+                print("Resume song")
+                resumeSong(driver, musicWindow)
+            elif "/help" in actualChatText:
+                print("Bot help")
+                helpCommand(driver)
+            elif "/exit" in actualChatText:
+                run=0
+                print("Exit bot")
+                driver.quit()
