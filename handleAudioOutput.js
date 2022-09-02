f71d96f629e37b393ba1ae6d407b49bd78e834b2
theoopsguy <theoopsguy01@gmail.com>
2022-09-02 15:49:02 +0530

    Add BlackHole input device, for mac devices, to audio output sink update function

--- handleAudioOutput.js
+++ handleAudioOutput.js
@@ -8,7 +8,7 @@ function inspect_devices(deviceInfos) {
   console.log("Inspecting Devices: " + deviceInfos.length + " device(s) total (audio/video input/output)");
   for (var deviceIdx = 0; deviceIdx < deviceInfos.length; deviceIdx++) {
     var deviceInfo = deviceInfos[deviceIdx];
-    if (deviceInfo.label.includes("VB-Audio") && (deviceInfo.kind == "audiooutput")) {
+    if ((deviceInfo.kind == "audiooutput") && (deviceInfo.label.includes("VB-Audio") || deviceInfo.label.includes("BlackHole"))) {
       sink_id = deviceInfo.deviceId;
       update_all_sinks();
     }
