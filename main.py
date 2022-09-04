66671ffe11f669a012027cc61cad03c5b0865a12
theoopsguy <theoopsguy01@gmail.com>
2022-09-05 00:28:06 +0530

    Integrate gui with bot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
+import tkinter as tk
 
 from joinMeet import turnMicCamOff, joinNow
 from commands import commands
@@ -19,20 +20,75 @@ opt.add_experimental_option("prefs", {
     "profile.default_content_setting_values.notifications": 1,
 })
 
-# Starting browser
-driver= webdriver.Chrome(executable_path=r"C:\Program Files (x86)\chromedriver_win32\chromedriver.exe",options=opt)
-print("browser start")
+root = tk.Tk()
+root.geometry("500x400")
+root.minsize(400, 380)
+root.title("Jitsi Music Bot")
 
-# Meeting link goes here
-driver.get('https://meet.jit.si/EmptyControlsMapAccidentally')
-driver.implicitly_wait(100)
-time.sleep(1)
+#logo
+logo = tk.PhotoImage(file="jitsi-logo-blue-grey-text.png")
+logoImage = tk.Label(image=logo)
+logoImage.pack(pady=10)
 
-# Joining meet
-turnMicCamOff(driver)
-driver.implicitly_wait(100)
-joinNow(driver)
-driver.implicitly_wait(100)
-time.sleep(1)
-# Accessing chat and following commands
-commands(driver)
+#title
+heading = tk.Label(root, text="Jitsi Music Bot", font="opensans 20 bold", pady=10)
+heading.pack()
+
+#description
+description = tk.Label(root, text="A simple music bot for Jitsi", font="opensans 10", pady=10)
+description.pack()
+
+#Variables
+meetLinkVar = tk.StringVar()
+chromedriverPathVar = tk.StringVar()
+botState = False
+meetLink=""
+chromedriverPath=""
+
+#function
+def bot(event):
+    if botState==False:
+        start()
+
+def start():
+    meetLink=(meetLinkVar.get())
+    chromedriverPath=(chromedriverPathVar.get())
+    btnText.set("Stop Bot")
+    botState = True
+    # Starting browser
+    driver= webdriver.Chrome(executable_path=chromedriverPath,options=opt)
+    print("browser start")
+
+    driver.get(meetLink)
+    driver.implicitly_wait(100)
+    time.sleep(1)
+
+    # Joining meet
+    turnMicCamOff(driver)
+    driver.implicitly_wait(100)
+    joinNow(driver)
+    driver.implicitly_wait(100)
+    time.sleep(1)
+    # Accessing chat and following commands
+    commands(driver)
+
+#meet link input
+meetLinkLabel = tk.Label(root, text="Enter the meet link", font="opensans 10", pady=10)
+meetLinkLabel.pack()
+meetLinkInput = tk.Entry(root, width=50, textvariable=meetLinkVar)
+meetLinkInput.pack()
+
+#path to chromedriver input
+chromedriverPathLabel = tk.Label(root, text="Enter the path to chromedriver", font="opensans 10", pady=10)
+chromedriverPathLabel.pack()
+chromedriverPathInput = tk.Entry(root, width=50, textvariable=chromedriverPathVar)
+chromedriverPathInput.pack()
+
+#submitButton
+btnText = tk.StringVar()
+submitButton = tk.Button(root, textvariable=btnText, font="opensans", bg="#1D76BC", fg="white", height=1, width=12)
+btnText.set("Start Bot")
+submitButton.pack(pady=20)
+submitButton.bind("<Button-1>", bot)
+
+root.mainloop()
